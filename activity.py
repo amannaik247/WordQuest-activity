7f407f273282874433d608c6b1af9c1283e656ac
amannaik247 <amancodes247@gmail.com>
2025-01-11 21:21:20 +0530

    modified:   activity.py 	modified:   words.txt

--- activity.py
+++ activity.py
@@ -1,24 +1,26 @@
 import gi
 import random
+import os
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GdkPixbuf
-from gettext import gettext as _
-
+from gi.repository import Gtk, Gdk
 from sugar3.activity import activity
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import StopButton
 from sugar3.activity.widgets import ActivityToolbarButton
-import random
-# from sprites import Sprites, Sprite 
 
 class WordleActivity(activity.Activity):
     def __init__(self, handle):
         super().__init__(handle)  # Call the parent constructor with the handle
         self.set_title("Wordle Clone")
+        self.set_default_size(400, 500)
+
+        self.word_list = self.load_words("words.txt")
+        self.secret_word = random.choice(self.word_list).upper()
+        self.attempts = 6
+        self.current_attempt = 0
 
         # Set up the toolbar
-        self.max_participants = 1
         toolbar_box = ToolbarBox()
         activity_button = ActivityToolbarButton(self)
         toolbar_box.toolbar.insert(activity_button, 0)
@@ -36,30 +38,22 @@ class WordleActivity(activity.Activity):
 
         self.set_toolbar_box(toolbar_box)
         toolbar_box.show()
-        
-        
-        #Code for game
-        self.set_default_size(400, 500)
-        
-        self.word_list = self.load_words("words.txt")
-        self.secret_word = random.choice(self.word_list).upper()
-        self.attempts = 6
-        self.current_attempt = 0
-        
+
+        # Main game layout
         self.grid = Gtk.Grid()
         self.add(self.grid)
-        
+
         self.entry = Gtk.Entry()
         self.entry.set_max_length(len(self.secret_word))  # Limit input length
         self.grid.attach(self.entry, 0, 0, 2, 1)
-        
+
         self.submit_button = Gtk.Button(label="Submit")
         self.submit_button.connect("clicked", self.check_guess)
         self.grid.attach(self.submit_button, 2, 0, 1, 1)
-        
+
         self.result_label = Gtk.Label()
         self.grid.attach(self.result_label, 0, 1, 3, 1)
-        
+
         self.guess_labels = []
         for i in range(self.attempts):
             label = Gtk.Label()
@@ -71,18 +65,22 @@ class WordleActivity(activity.Activity):
         self.show_all()
 
     def load_words(self, filename):
+        """Load words from a specified file."""
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(f"Word list file '{filename}' not found.")
         with open(filename, 'r') as file:
             return [line.strip() for line in file.readlines()]
 
     def check_guess(self, widget):
+        """Check the user's guess against the secret word."""
         guess = self.entry.get_text().upper()
         if len(guess) != len(self.secret_word):
             self.result_label.set_text("Invalid guess length!")
             return
-        
+
         self.current_attempt += 1
         self.update_guesses(guess)
-        
+
         if guess == self.secret_word:
             self.result_label.set_text("Congratulations! You've guessed the word!")
             self.submit_button.set_sensitive(False)
@@ -91,13 +89,14 @@ class WordleActivity(activity.Activity):
             self.submit_button.set_sensitive(False)
         else:
             self.result_label.set_text("Try again!")
-        
+
         self.entry.set_text("")  # Clear the entry after submission
 
     def update_guesses(self, guess):
+        """Update the displayed guesses and color code them."""
         guess_label = self.guess_labels[self.current_attempt - 1]
         guess_label.set_text(guess)
-        
+
         # Color coding logic
         for i, letter in enumerate(guess):
             if letter == self.secret_word[i]:
